7f69978bc6d8fbd484d5332f5360c00ac957fb32
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-06-11 11:08:04 -0300

    Add files via upload

---
+++ 1-array.js
@@ -0,0 +1,4 @@
+const numeros = [1, 2, 3, 4.4, 5]
+numeros.pop()
+
+console.log(numeros)
---
+++ 2-array.js
@@ -0,0 +1,4 @@
+const fruta = ['banana', 'maça', 'laranja', 'abacate', 'melancia', 'morango']
+const tiraElemento = fruta.slice(1) 
+
+console.log("Lista após retirar o primeiro elemento usando slice:", tiraElemento);
