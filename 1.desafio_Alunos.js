c2deee1637d2847a1c7d1264dd5eae4e9d2ef4bb
Théfa <140509878+Thefa07@users.noreply.github.com>
2024-07-25 11:34:30 -0300

    Add files via upload

---
+++ 1.desafio_Alunos.js
@@ -0,0 +1,5 @@
+const alunos = ["Maria, João, Mileni, Beatriz, Mabe, Teresa, Ana, Emilly"]
+const alunosMaiusculos = alunos.map(()=>{
+    return alunos.toUpeercase();
+})
+console.log(alunosMaiusculos)
